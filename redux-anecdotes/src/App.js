6eff809f1ac86b4f8ba86b7bf5703e83b444f661
Fynis96 <C.hearth96@gmail.com>
2023-06-28 17:21:10 -0400

    implemented mock backend service using json-server, all anecdotes fetched and posted to mock db

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
 import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AncedoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
-
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+  
   return (
     <div>
       <h2>Anecdotes</h2>
